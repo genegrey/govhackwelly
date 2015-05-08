01a3b2d94bf73bff303b263f39dd50e54d23df1c
dane foster <dene@slush.ca>
2015-05-09 08:47:22 +1200

    * revert gh pages test

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://denefoster.github.io/govhackwelly'
+SITEURL = 'http://genegray.github.io/govhackwelly'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
